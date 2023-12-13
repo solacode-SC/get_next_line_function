773fc384e09921b23a22d20124848aee419fb6c9
SolaCode <117476632+solacode-SC@users.noreply.github.com>
2023-12-13 22:19:24 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soel-mou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 21:35:47 by soel-mou          #+#    #+#             */
+/*   Updated: 2023/12/13 21:35:54 by soel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int		ft_printf(const char *input, ...);
+int		print_pointer(unsigned long value, int asc);
+int		print_unsigned(unsigned int nb);
+int		print_string(char *s);
+int		print_int(int n);
+int		print_char(char c);
+int		print_hex(unsigned int value, int asc);
+int     ft_strlen(const char *str);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+#endif
